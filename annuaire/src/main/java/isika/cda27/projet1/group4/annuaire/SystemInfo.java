8d54b2e8e3c2f76f4fc4f23540f0610b31fb3bc4
JohannGourmelon <johann.gourmelon@gmail.com>
2024-08-21 14:17:28 +0200

    initialisation maven

---
+++ annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/SystemInfo.java
@@ -0,0 +1,13 @@
+package isika.cda27.projet1.group4.annuaire;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ annuaire/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module isika.cda27.projet1.group4.annuaire {
+    requires javafx.controls;
+    exports isika.cda27.projet1.group4.annuaire;
+}
