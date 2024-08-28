9443e7e8d58e96ea59efd940a69e47d2f8cdd076
motiroir <contact.tiroirmorgane@gmail.com>
2024-08-28 19:06:33 +0200

    UI with link to methods

--- annuaire/src/main/java/module-info.java
+++ annuaire/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module isika.cda27.projet1.group4.annuaire {
     requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.base;
     exports isika.cda27.projet1.group4.annuaire;
     exports isika.cda27.projet1.group4.annuaire.back;
 }
