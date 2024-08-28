e67a1ce9d2bc29cb24a7e676e439ef01c6afb509
motiroir <contact.tiroirmorgane@gmail.com>
2024-08-28 22:58:34 +0200

    ui ok & update page ok

--- annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/front/Footer.java
+++ annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/front/Footer.java
@@ -13,26 +13,46 @@ import javafx.stage.Stage;
 public class Footer extends HBox {
 
 	private HomePage homePage;
-	
+
 	public Footer(App app, Stage stage, ObservableList<Stagiaire> myObservableArrayList, HomePage homePage) {
 
-		Button buttonUpdate = new Button(" Modifier ");
+		Button buttonUpdate = new Button("Modifier");
 		this.getChildren().add(buttonUpdate);
-		this.setMargin(buttonUpdate, new Insets(20, 0, 20, 80));// Marges haut, droite, bas, gauche
+		this.setMargin(buttonUpdate, new Insets(20, 0, 20, 80));
 
-		Button buttonDelete = new Button(" Supprimer ");
+		Button buttonDelete = new Button("Supprimer");
 		this.getChildren().add(buttonDelete);
-		this.setMargin(buttonDelete, new Insets(20, 0, 20, 80));// Marges haut, droite, bas, gauche
+		this.setMargin(buttonDelete, new Insets(20, 0, 20, 80));
 
-		Button buttonAdd = new Button("  Ajouter ");
+		Button buttonAdd = new Button(" Ajouter");
 		this.getChildren().add(buttonAdd);
-		this.setMargin(buttonAdd, new Insets(20, 0, 20, 80));// Marges haut, droite, bas, gauche
+		this.setMargin(buttonAdd, new Insets(20, 0, 20, 80));
 
-		Button buttonImpression = new Button("Impression (pdf)");
+		Button buttonImpression = new Button("Imprimer");
 		this.getChildren().add(buttonImpression);
-		this.setMargin(buttonImpression, new Insets(20, 0, 20, 530));// Marges haut, droite, bas, gauche
+		this.setMargin(buttonImpression, new Insets(20, 0, 20, 530));
 
-		// méthode du bouton add
+		// Desactivation des boutons initial
+		buttonUpdate.setDisable(true);
+		buttonDelete.setDisable(true);
+
+		// Utiliser la méthode pour obtenir TableViewStagiaires
+		TableViewStagiaires tableView = homePage.getTableView();
+
+		// Écouter les changements de sélection dans le TableView
+		tableView.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+			if (newSelection != null) {
+				// Activer les boutons si un élément est sélectionné
+				buttonDelete.setDisable(false);
+				buttonUpdate.setDisable(false);
+			} else {
+				// Désactiver les boutons si aucune sélection
+				buttonDelete.setDisable(true);
+				buttonUpdate.setDisable(true);
+			}
+		});
+
+		// Méthode du bouton add
 		buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -42,21 +62,33 @@ public class Footer extends HBox {
 			}
 		});
 
-		// méthode du bouton delete
+		// Méthode du bouton delete
 		buttonDelete.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
-	        public void handle(ActionEvent event) {
+			public void handle(ActionEvent event) {
 				// Utiliser la méthode pour obtenir TableViewStagiaires
-                TableViewStagiaires tableView = homePage.getTableView();
-	            // Récupérer le stagiaire sélectionné
-	            Stagiaire selectedStagiaire = tableView.getSelectionModel().getSelectedItem();
-	            if (selectedStagiaire != null) {
-	                // Supprimer le stagiaire
-	                app.myDAO.removeStagiaire(selectedStagiaire);
-	                myObservableArrayList.setAll(app.myDAO.getStagiaires());
-	            }
-	        }
-			
+				TableViewStagiaires tableView = homePage.getTableView();
+				// Récupérer le stagiaire sélectionné
+				Stagiaire selectedStagiaire = tableView.getSelectionModel().getSelectedItem();
+				if (selectedStagiaire != null) {
+					// Supprimer le stagiaire
+					app.myDAO.removeStagiaire(selectedStagiaire);
+					myObservableArrayList.setAll(app.myDAO.getStagiaires());
+				}
+			}
+
+		});
+
+		buttonUpdate.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Stagiaire selectedStagiaire = tableView.getSelectionModel().getSelectedItem();
+				UpdateForm updateForm = new UpdateForm(app, stage, selectedStagiaire);
+				stage.setScene(updateForm.getScene());
+				stage.setTitle("Page de modification");
+
+			}
 		});
 
 	}
