90173cf5936ad371dcc75a96668624e8d012a3fc
JohannGourmelon <johann.gourmelon@gmail.com>
2024-08-29 14:49:17 +0200

    save changes

--- annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/front/UserConnexion.java
+++ annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/front/UserConnexion.java
@@ -103,7 +103,28 @@ public class UserConnexion extends BorderPane {
 		validateButton.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				// Récupérer les valeurs des champs
+				
+				 boolean isValid = true;
+
+			        // Vérification pour le champ nameTextfield
+			        if (usernameField.getText().isEmpty()) {
+			        	usernameField.getStyleClass().add("text-field-error");
+			            isValid = false;
+			        } else {
+			        	usernameField.getStyleClass().remove("text-field-error");
+			        }
+
+			        // Vérification pour le champ firstnameTextfield
+			        if (passwordField.getText().isEmpty()) {
+			        	passwordField.getStyleClass().add("text-field-error");
+			            isValid = false;
+			        } else {
+			        	passwordField.getStyleClass().remove("text-field-error");
+			        }
+
+			     // Si tous les champs sont valides
+		        if (isValid) {
+		        // Récupérer les valeurs des champs
 				String username = usernameField.getText();
 	            String password = passwordField.getText();
 	            app.currentUser = app.userManager.authenticate(username, password);
@@ -116,6 +137,7 @@ public class UserConnexion extends BorderPane {
 	                Alert alert = new Alert(Alert.AlertType.ERROR, "Nom d'utilisateur ou mat de passe invalide");
 	                alert.show();
 	            }
+		        }
 			}
 		});
 		
