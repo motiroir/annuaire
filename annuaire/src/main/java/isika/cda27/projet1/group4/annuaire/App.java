6220102c87d1c356d89654eba6085326f5a05cab
JohannGourmelon <johann.gourmelon@gmail.com>
2024-08-30 18:32:39 +0200

    commit de vendredi soir

--- annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/App.java
+++ annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/App.java
@@ -1,41 +1,16 @@
 package isika.cda27.projet1.group4.annuaire;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.util.Date;
-import java.util.List;
 
-import isika.cda27.projet1.group4.annuaire.back.Annuaire;
-import isika.cda27.projet1.group4.annuaire.back.BinarySearchTree;
 import isika.cda27.projet1.group4.annuaire.back.FileChecker;
-import isika.cda27.projet1.group4.annuaire.back.Node;
 import isika.cda27.projet1.group4.annuaire.back.Stagiaire;
 import isika.cda27.projet1.group4.annuaire.back.User;
 import isika.cda27.projet1.group4.annuaire.back.UserManager;
 import isika.cda27.projet1.group4.annuaire.front.AnnuaireDAO;
-import isika.cda27.projet1.group4.annuaire.front.Header;
 import isika.cda27.projet1.group4.annuaire.front.HomePage;
-import isika.cda27.projet1.group4.annuaire.front.TableViewStagiaires;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -73,7 +48,8 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
-
+		
+		stage.getIcons().setAll(new Image(getClass().getResource("/icons/contacter-le-carnet.png").toExternalForm()));
 		myObservableArrayList = FXCollections.observableArrayList(this.myDAO.getStagiaires());
 
 		// Création de la première scène
