2f6dd52f5d47846aa17c1d31d0e4568e53a8b34f
JohannGourmelon <johann.gourmelon@gmail.com>
2024-08-30 12:07:05 +0200

    userManager et users en liste ok

---
+++ annuaire/src/main/java/isika/cda27/projet1/group4/annuaire/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package isika.cda27.projet1.group4.annuaire.exceptions;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
+
